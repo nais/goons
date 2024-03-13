a6f2ea341ee297d455121be6283f017f56b6e68f
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-03-13 14:22:06 +0100

    Remove organizationID and update folderIDs handling

--- internal/cmd/goons.go
+++ internal/cmd/goons.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/nais/goons/internal/gke"
 	"github.com/sirupsen/logrus"
@@ -11,14 +12,12 @@ import (
 )
 
 var cfg struct {
-	orgID        string
 	folderIDs    string
 	slackWebhook string
 }
 
 func init() {
-	flag.StringVar(&cfg.orgID, "orgID", os.Getenv("ORGANIZATION_ID"), "GCP Organzation ID")
-	flag.StringVar(&cfg.folderIDs, "folderIDs", os.Getenv("FOLDER_ID"), "GCP Folder")
+	flag.StringVar(&cfg.folderIDs, "folderIDs", os.Getenv("FOLDER_ID"), "GCP Folders - delimited by comma")
 	flag.StringVar(&cfg.slackWebhook, "slackWebhook", os.Getenv("SLACK_WEBHOOK"), "Slack Webhook")
 }
 
@@ -26,17 +25,18 @@ func Run(ctx context.Context) {
 	log := logrus.StandardLogger()
 	flag.Parse()
 
-	if cfg.orgID == "" && cfg.folderIDs == "" {
-		log.Fatal("Organization ID or Folder ID must be set")
+	if cfg.folderIDs == "" {
+		log.Fatal("Folder IDs must be set")
 	}
 
-	sccClient, err := gke.New(ctx, log)
-	if err != nil {
-		log.Fatal(err)
-	}
+	for _, folder := range strings.Split(cfg.folderIDs, ",") {
+		sccClient, err := gke.New(ctx, log)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Infof("fetching findings for folder: %s", folder)
 
-	if cfg.orgID != "" {
-		result, err := sccClient.ListFindings(ctx, fmt.Sprintf("organizations/%s/sources/-", cfg.orgID))
+		result, err := sccClient.ListFindings(ctx, fmt.Sprintf("folders/%s/sources/-", folder))
 		if err != nil {
 			log.Fatal(err)
 		}
